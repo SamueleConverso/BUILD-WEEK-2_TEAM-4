0259a31483717aaa1a7b42d7a44881763adb3f0d
Samuele Converso <samu.converso@gmail.com>
2024-12-18 16:46:30 +0100

    .

--- assets/js/album.js
+++ assets/js/album.js
@@ -15,6 +15,8 @@ const albumTitleDown = document.getElementById("albumTitleDown");
 const artistNameDown = document.getElementById("artistNameDown");
 const trackList = document.getElementById("trackList");
 
+let btnSongToPlay;
+
 let album;
 let tracks;
 let artistName;
@@ -73,7 +75,7 @@ function printData() {
     let duration = (tracks[i].duration / 60).toFixed(2);
     newLi.innerHTML = `
                   <div class="col-5 titleSong">
-                    <p class="fw-bold mb-1">${tracks[i].title}</p>
+                    <p id="${tracks[i].preview}" class="fw-bold mb-1 btn btn-success bg-transparent border-0 p-0 m-0 text-success text-start btnSongToPlay">${tracks[i].title}</p>
                     <p class="fw-lighter mb-1">${artistName}</p>
                   </div>
                   <p class="col-3">122.631.768</p>
@@ -82,6 +84,7 @@ function printData() {
     `;
     trackList.appendChild(newLi);
   }
+  addClickToSong();
 }
 
 function getArtistPhoto() {
@@ -121,4 +124,18 @@ function getArtistPhoto() {
       artistPhoto = album.artist.picture_small;
   }
   return artistPhoto;
+}
+
+function addClickToSong() {
+  btnSongToPlay = document.querySelectorAll(".btnSongToPlay");
+  btnSongToPlay.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      e.preventDefault();
+      loadSong(btn.id);
+    });
+  });
+}
+
+function loadSong(preview) {
+  console.log(preview);
 }
