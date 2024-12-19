96a4e8bf3400f7a7822ffdfc5c9fe0d2e76baffb
valentinarizzo21 <rizzo.val3ntina@gmail.com>
2024-12-19 11:35:09 +0100

    header index modificata, implementato js pagine, micromodifiche css

--- assets/js/album.js
+++ assets/js/album.js
@@ -169,6 +169,15 @@ function getArtistPhoto() {
   return artistPhoto;
 }
 
+btnSearch.addEventListener("click", (e) => {
+  e.preventDefault();
+  let inputValue = inputSearch.value;
+  //getData(inputValue);
+  let secondPage = "artist.html";
+  let newUrl = `${secondPage}?_searched-query=${inputValue}`;
+  window.location.href = newUrl;
+});
+
 /*------ FUNZIONI PLAYER -------*/
 playButton.addEventListener("click", (e) => {
   e.preventDefault();
--- assets/js/artist.js
+++ assets/js/artist.js
@@ -211,3 +211,12 @@ function getArtistPhoto() {
   }
   return artistPhoto;
 }
+
+btnSearch.addEventListener("click", (e) => {
+  e.preventDefault();
+  let inputValue = inputSearch.value;
+  //getData(inputValue);
+  let secondPage = "artist.html";
+  let newUrl = `${secondPage}?_searched-query=${inputValue}`;
+  window.location.href = newUrl;
+});
